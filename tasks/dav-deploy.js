6aa6cc4f6e2b36a6d65f6c4519d1777b3f0fb9d0
Karl Glatz <karl@glatz.biz>
2018-11-09 13:40:48 +0100

    update dependencies and upload file with full path

--- tasks/dav-deploy.js
+++ tasks/dav-deploy.js
@@ -15,12 +15,12 @@ module.exports = function(grunt) {
 			overwrite: false
 		});
 
-		grunt.log.debug('Preparing copy...');
+		grunt.log.debug('Preparing copy...', this.files);
 
 		var rc = JSON.parse(fs.readFileSync(path.resolve(process.cwd(), '.simpledavrc'))),
 			wfs = webdav(options.urlBase, encodeURIComponent(rc.user), encodeURIComponent(rc.passwd)),
 			done = this.async(),
-			files = this.files.map(function(file) {return file.src[0];}),
+			files = this.files.map(function(file) {return file.src;})[0], // TODO: flattern?
 			filesLength = files.length,
 			transferFiles = function(err) {
 				if(err) {
@@ -31,22 +31,53 @@ module.exports = function(grunt) {
 
 				async.waterfall(files.map(function(file) {
 					return function(callback) {
-						var wfs = webdav(options.urlBase, rc.user, rc.passwd);
-						wfs.writeFile(path.join(options.target, path.basename(file)), fs.readFileSync(file), 'utf8', function(err) {
-							if(err)
-								return callback(err);
-							grunt.log.writeln('Copying: ' + path.basename(file));
-							callback();
+						grunt.log.debug('path', options.target, file);
+
+						var currentFileTargetDir = path.dirname(path.join(options.target, file));
+
+						var transferNextFile = function (err) {
+							if(err) {
+								grunt.log.error(err);
+								return done(false);
+							}
+							grunt.log.debug('Write File', path.join(options.target, file));
+							// TODO: why can't we use encodeUIRComponent here?
+							var wfs2 = webdav(options.urlBase, rc.user, rc.passwd);
+							// skip directories (created before)
+							if(fs.statSync(file).isDirectory()) {
+								callback();
+								return;
+							}
+							wfs2.writeFile(path.join(options.target, file), fs.readFileSync(file), 'utf8', function(err) {
+								if(err)
+									return callback(err);
+								grunt.log.writeln('Copying: ' + file);
+								callback();
+							});
+						};
+
+						wfs.readdir(currentFileTargetDir, function(err, result) {
+							if(err && err.httpStatusCode && err.httpStatusCode === 404) {
+								grunt.log.debug('Creating new folder', currentFileTargetDir);
+								wfs.mkdir(currentFileTargetDir, transferNextFile);
+							} else {
+								grunt.log.debug('Folder exists', currentFileTargetDir);
+								transferNextFile();
+							}
 						});
+
 					};
 				}), done);
 			};
 
 		grunt.log.debug('Connected, file paths ready...');
-		grunt.log.debug('Copying files: ' + files.join('\n'));
+		grunt.log.debug('Copying files: ', files);
 		grunt.log.debug('To: ' + url.resolve(options.urlBase + '/', options.target));
-		
-		wfs.readdir(options.target, function(err, result) {
+
+		// go
+		transferFiles();
+
+		/*wfs.readdir(options.target, function(err, result) {
 			if(err && err.httpStatusCode && err.httpStatusCode === 404) {
 				grunt.log.debug('Creating new folder...');
 				wfs.mkdir(options.target, transferFiles);
@@ -69,6 +100,6 @@ module.exports = function(grunt) {
 				return done(false);
 			}
 
-		});
+		});*/
 	});
 };
