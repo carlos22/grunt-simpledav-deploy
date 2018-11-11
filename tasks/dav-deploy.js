d07bb9612bfe4326d1589d4aa0e02ec5842308fe
Karl Glatz <karl@glatz.biz>
2018-11-11 20:39:46 +0100

    fix creation of subfolders if parent does not exist (mkdir -p)

--- tasks/dav-deploy.js
+++ tasks/dav-deploy.js
@@ -6,6 +6,51 @@ var webdav = require('webdav-fs'),
 	async = require('async'),
 	url = require('url');
 
+// webdav version based on https://github.com/substack/node-mkdirp/blob/master/index.js
+function mkdirP (p, opts, f, made) {
+	if (typeof opts === 'function') {
+		f = opts;
+		opts = {};
+	}
+	else if (!opts || typeof opts !== 'object') {
+		opts = { mode: opts };
+	}
+
+	var mode = opts.mode; // unused
+	var xfs = opts.fs || fs;
+
+	if (!made) made = null;
+
+	var cb = f || function () {};
+
+	xfs.mkdir(p, function (er) {
+	opts.grunt.log.debug('mkdirP', er);
+		if (!er) {
+			made = made || p;
+			return cb(null, made);
+		}
+		switch (er.status) {
+			case 409:
+				mkdirP(path.dirname(p), opts, function (er, made) {
+					if (er) cb(er, made);
+					else mkdirP(p, opts, cb, made);
+				});
+				break;
+
+			// In the case of any other error, just see if there's a dir
+			// there already.  If so, then hooray!  If not, then something
+			// is borked.
+			default:
+				xfs.stat(p, function (er2, stat) {
+					// if the stat fails, then that's super weird.
+					// let the original error be the failure reason.
+					if (er2 || !stat.isDirectory()) cb(er, made)
+					else cb(null, made);
+				});
+				break;
+		}
+	});
+}
 
 module.exports = function(grunt) {
 	var options;
@@ -17,7 +62,7 @@ module.exports = function(grunt) {
 		grunt.log.debug('Preparing copy...', this.files);
 
 		var rc = JSON.parse(fs.readFileSync(path.resolve(process.cwd(), '.simpledavrc'))),
-			wfs = webdav(options.urlBase, encodeURIComponent(rc.user), encodeURIComponent(rc.passwd)),
+			//wfs = webdav(options.urlBase, encodeURIComponent(rc.user), encodeURIComponent(rc.passwd)),
 			done = this.async(),
 			files = this.files.map(function(file) {return file.src;})[0], // TODO: flattern?
 			filesLength = files.length,
@@ -30,10 +75,7 @@ module.exports = function(grunt) {
 
 				async.waterfall(files.map(function(file) {
 					return function(callback) {
-						grunt.log.debug('path', options.target, file);
-
-						var currentFileTargetDir = path.dirname(path.join(options.target, file));
-
+						// transfer file (called from below)
 						var transferNextFile = function (err) {
 							if(err) {
 								grunt.log.error(err);
@@ -55,11 +97,19 @@ module.exports = function(grunt) {
 								callback();
 							});
 						};
-
+						// check if dirname of current file exists
+						var wfs = webdav(options.urlBase, rc.user, rc.passwd);
+						var currentFileTargetDir = path.dirname(path.join(options.target, file));
+						grunt.log.debug('Read currentFileTargetDir,', currentFileTargetDir);
 						wfs.readdir(currentFileTargetDir, function(err, result) {
-							if(err && err.httpStatusCode && err.httpStatusCode === 404) {
-								grunt.log.debug('Creating new folder', currentFileTargetDir);
-								wfs.mkdir(currentFileTargetDir, transferNextFile);
+							if(err) {
+								// create folder if it does not exist
+								if(err.status && err.status === 404) {
+									grunt.log.debug('Creating new folder', currentFileTargetDir);
+									mkdirP(currentFileTargetDir, {fs: wfs}, transferNextFile);
+								} else {
+									grunt.log.error(err);
+								}
 							} else {
 								grunt.log.debug('Folder exists', currentFileTargetDir);
 								transferNextFile();
